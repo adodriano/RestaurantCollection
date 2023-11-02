5f6c2825c0a8f48310a3c8d3d76bae4a053d0804
adodriano <adodriano@gmail.com>
2023-11-01 22:19:49 -0300

    Publicacao

---
+++ RestaurantCollection/Controllers/RestaurantController.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using RestaurantCollection.WebApi.DataAccess;
+//using RestaurantCollection.WebApi.DTO.Common;
+using RestaurantCollection.WebApi.DTO.Forms;
+//using RestaurantCollection.WebApi.DTO.ViewModels;
+using RestaurantCollection.WebApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace RestaurantCollection.WebApi.Controllers
+{
+    [ApiController]
+    [Route("api/")]
+    public class RestaurantController : ControllerBase
+    {
+        private readonly IRepository _repository;
+
+        public RestaurantController(IRepository repository)
+        {
+            _repository = repository;
+
+        }
+
+        [HttpPost]
+        [Route("restaurant")]
+        public async Task<ActionResult<Restaurant>> Post([FromBody] CreateForm restaurantForm)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            var newRestaurant = new Restaurant
+            {
+                Name = restaurantForm.Name,
+                City = restaurantForm.City,
+                EstimatedCost = restaurantForm.Cost,
+                AverageRating = restaurantForm.Rating,
+                Votes = restaurantForm.Votes
+            };
+            try
+            {
+                await _repository.AddRestaurant(newRestaurant);
+
+                return Created(uri: $"api/restaurante/{newRestaurant.Id}", newRestaurant);
+            }
+            catch (Exception ex)
+            {
+
+                return BadRequest(ex);
+            }
+        }
+
+
+        [HttpPut]
+        [Route("restaurant/{id}")]
+        public async Task<ActionResult<Restaurant>> UpdateRestaurant([FromRoute] int id, [FromBody] UpdateForm updateForm)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            var restaurant = await _repository.GetRestaurantById(id);
+
+            if (restaurant == null)
+            {
+                return NotFound();
+            }
+
+            restaurant.AverageRating = updateForm.Rating;
+            restaurant.Votes = updateForm.Votes;
+
+            var updatedRestaurant = await _repository.UpdateRestaurant(id, updateForm);
+
+            return Ok(updatedRestaurant);
+        }
+
+
+        [HttpGet]
+        [Route("restaurant")]
+        public async Task<ActionResult<List<Restaurant>>> Get()
+        {
+
+            var restaurants = await _repository.GetRestaurants();
+
+            return Ok(restaurants);
+
+        }
+
+        [HttpGet("restaurant/query")]
+        public async Task<ActionResult<List<Restaurant>>> GetRestaurantsByCity([FromQuery] string city)
+        {
+            try
+            {
+                var restaurants = await _repository.GetRestaurants(new RestaurantQueryModel { City = city });
+                return Ok(restaurants);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+        }
+
+        [HttpGet]
+        [Route("restaurant/{id}")]
+        public async Task<ActionResult<Restaurant>> GetById([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            var restaurant = await _repository.GetRestaurantById(id);
+
+            if (restaurant == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(restaurant);
+        }
+
+        [HttpDelete]
+        [Route("restaurant/{id}")]
+        public async Task<ActionResult<Restaurant>> DeleteRestaurant(int id)
+        {
+            var restaurant = await _repository.DeleteRestaurant(id);
+
+            if (restaurant == null)
+            {
+                return NotFound();
+            }
+
+            return NoContent();
+        }
+
+
+        [HttpGet("restaurant/sort")]
+        public async Task<ActionResult<List<Restaurant>>> GetSortedRestaurants()
+        {
+            try
+            {
+                var sortedRestaurants = await _repository.GetRestaurantsSorted();
+                return Ok(sortedRestaurants);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+        }
+
+    }
+}
---
+++ RestaurantCollection/DataAccess/Repository.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using RestaurantCollection.WebApi.DTO.Forms;
+using RestaurantCollection.WebApi.Models;
+
+namespace RestaurantCollection.WebApi.DataAccess
+{
+    /// <summary>
+    /// Repository
+    /// </summary>
+    public class Repository : IRepository
+    {
+        private readonly RestaurantCollectionContext _context;
+
+        public Repository(RestaurantCollectionContext context)
+        {
+            _context = context;
+        }
+
+        /// <inheritdoc />
+        public async Task<List<Restaurant>> GetRestaurants()
+        {
+            IQueryable<Restaurant> restaurants = _context.Restaurants;
+            // return await restaurants.OrderByDescending(o => o.Id).ToListAsync();
+            return await restaurants.OrderBy(o => o.Id).ToListAsync();
+        }
+
+        /// <inheritdoc />
+        public async Task<Restaurant> AddRestaurant(Restaurant restaurant)
+        {
+            await _context.Restaurants.AddAsync(restaurant);
+            await _context.SaveChangesAsync();
+
+            return restaurant;
+        }
+
+        public async Task<Restaurant> DeleteRestaurant(int id)
+        {
+            var restaurant = await _context.Restaurants.FindAsync(id);
+            _context.Restaurants.Remove(restaurant);
+            await _context.SaveChangesAsync();
+
+            return restaurant;
+        }
+
+        public async Task<List<Restaurant>> GetRestaurantsSorted()
+        {
+            IQueryable<Restaurant> restaurants = _context.Restaurants;
+            return await restaurants.OrderByDescending(o => o.AverageRating).ToListAsync();
+        }
+
+        public async Task<List<Restaurant>> GetRestaurants(RestaurantQueryModel query)
+        {
+            IQueryable<Restaurant> restaurants = _context.Restaurants;
+            if (query.City != null)
+            {
+                restaurants = restaurants.Where(o => o.City == query.City);
+            }
+
+            if (query.Id > 0)
+            {
+                restaurants = restaurants.Where(o => o.Id == query.Id);
+            }
+            return await restaurants.OrderBy(o => o.Id).ToListAsync();
+        }
+
+        public async Task<Restaurant> UpdateRestaurant(int id, UpdateForm updateForm)
+        {
+            var rest = await _context.Restaurants.FindAsync(id);
+            rest.AverageRating=updateForm.Rating;
+            rest.Votes = updateForm.Votes;
+            _context.Restaurants.Update(rest);
+            await _context.SaveChangesAsync();
+
+            return rest;
+        }
+
+        public async Task<Restaurant> GetRestaurantById(int id)
+        {
+            return await _context.Restaurants.FirstOrDefaultAsync(r => r.Id == id);
+        }
+
+    }
+}
